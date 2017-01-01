48702f82cb4f4a79675708bf8f92e36ae36bc0b1
Andrew Sheehan <sheehan.andrew@gmail.com>
2017-01-01 17:03:19 -0600

    added video models/views/controllers with user-scoped CRUD actions

---
+++ web/models/video.ex
@@ -0,0 +1,24 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+
+    timestamps
+  end
+
+  @required_fields ~w(url title description)a
+  @option_fields ~w()a
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
+end
